290ae0ca9bc45bb7f047d68cb2b5f6619c2f8a7e
lineulimasjc <lineulimasjc@gmail.com>
2015-08-12 00:08:40 -0300

    Adding pow.c file

---
+++ pow.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main()
+{
+    int x;
+
+    printf("Digite um numero: ");
+    scanf("%d", &x);
+
+
+
+
+    if (x == 1)
+    {
+        printf("Você digitou 1");
+    }
+    else
+    {
+        printf("Você NAO digitou 1");
+    }
+}
